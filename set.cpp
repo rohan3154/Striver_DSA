0ea4b0caf96c94da7f496a287ad2176dbf027b1e
rohan3154 <rohan31054@gmail.com>
2025-07-22 19:23:35 +0000

    Done

--- set.cpp
+++ set.cpp
@@ -12,29 +12,60 @@ void expalinSet() {
     // Functionality of insert in vector can be usedalso, that only increases efficiency
     // begin(), end(), rbegin(), rend(), size(), empty(), and swap() are same as those of above
 
-    // cout << "set value: ";
-    // for(int value : st) {
-    //     cout << value << " " ;
-    // }
-
-    int num;
-    cout << "Enter the number you find in Set: ";
-    cin >> num;
-
-    auto it = st.find(num);
-
-    if(it != st.end()) {
-        cout << "The number is find";
-    } else {
-        cout << "number is not found";
+    cout << "set value: ";
+    for(int value : st) {
+        cout << value << " ";
     }
 
-    
+    cout << endl;
+    // int num;
+    // cout << "Enter the number you find in Set: ";
+    // cin >> num;
+
+    // auto it = st.find(num);
+
+    // if(it != st.end()) {
+    //     cout << "The number " << num << " is find";
+    // } else {
+    //     cout << "number "<< num << " is not found";
+    // }
+
+    // st.erase(num);
+
+    //  cout << "set value after erace " << num;
+    // for(int value1 : st) {
+    //     cout << value1 << " " ;
+    // }
+    cout << endl;
+
+    int cnt = st.count(5); // using this we can count the number wich are come how many time
+    cout << cnt;
 
     cout << endl;
 }
 
+void explainMultiset() {
+    cout << "multiset" << endl;
+    multiset<int> ms;
+    ms.insert(8);
+    ms.insert(8);
+    ms.insert(8);
+
+    ms.erase(8);
+
+    cout<<ms.count(8)<<endl;
+}
+
+void explainUniset() {
+    unordered_set set<int> st;
+    // lowerbound and upperbound function
+    // dose not works, reset all functions are same
+    // as above, it has a better complexity
+    // than set in most cases, expect some when collision happen
+}
+
 int main() {
     expalinSet();
+    explainMultiset();
     return 0;
 }
